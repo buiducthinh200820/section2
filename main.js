b8b17aa96ee549c9687b2e3bb2401bd4954008db
buiducthinh200820 <101413818+buiducthinh200820@users.noreply.github.com>
2023-12-02 15:16:18 +0700

    update css,js

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-const arrow = document.querySelector('i');
-const content =document.getElementById ('content')
-arrow.addEventListener("click", () => {
-    if (content.style.display === "none") {
-        content.style.display = "block";
-        arrow.classList.remove("fa-chevron-down");
-        arrow.classList.add('fa-chevron-up')
-        console.log(arrow);
 
-      } else {
-        content.style.display = "none";
-        arrow.classList.add("fa-chevron-down");
-        arrow.classList.remove('fa-chevron-up')
-      }
+const arrows = document.querySelectorAll('.fa-solid.fa-chevron-down');
+const contents = document.querySelectorAll('.text-2');
+
+for (let i = 0; i < arrows.length; i++) {
+  arrows[i].addEventListener('click', () => {
+    const arrow = arrows[i];
+    const content = contents[i];
+
+    if (content.style.display === 'none') {
+      content.style.display = 'block';
+      arrow.classList.remove('fa-chevron-down');
+      arrow.classList.add('fa-chevron-up');
+    } else {
+      content.style.display = 'none';
+      arrow.classList.add('fa-chevron-down');
+      arrow.classList.remove('fa-chevron-up');
+    }
   });
- 
+}
